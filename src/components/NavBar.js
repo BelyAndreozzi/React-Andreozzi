1df96542fbf4f38c3620f7f51b668203f1749fab
Bely <fbelen.andreozzi@gmail.com>
2022-07-19 22:52:44 -0300

    NavBar 1era entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return(
+        <header>
+                <h1>Joyería</h1>
+                <nav>
+                    <a href="#">Collares</a>
+                    <a href="#">Anillos</a>
+                    <a href="#">Pulseras</a>
+                </nav>
+        </header> 
+    )
+}
+
+export default NavBar
